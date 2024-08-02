b7c0ebc14e5e1885aa78cc83995438273a5285cd
JStockwell <jstockwellmendoza@gmail.com>
2024-08-02 16:39:31 +0200

    Revert "feat: better combat simulation"
    This reverts commit ff29dbb2767c021e34f27d87ad342e98717463f3.

--- Scripts/combat_proto.gd
+++ Scripts/combat_proto.gd
@@ -30,49 +30,53 @@ var playerStatsLabel = $UI/StatsLabel/PlayerStatsLabel
 @onready
 var enemyStatsLabel = $UI/StatsLabel/EnemyStatsLabel
 
-var playerAttacked = false
+var isPlayerPhase = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	buttonAttack.disabled = true
-	
 	Player.set_stats(GameStatus.activePlayer)
 	Enemy.set_stats(GameStatus.activeEnemy)
 	
 	setStatsLabel(playerStatsLabel, GameStatus.activePlayer)
 	setStatsLabel(enemyStatsLabel, GameStatus.activeEnemy)
 	
+	buttonAttack.disabled = true
 	hpLabelEnemy.text = str(Enemy.get_health())
 	hpLabelPlayer.text = str(Player.get_health())
-	
 	change_phase()
+	
+func _input(event):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		emit_signal("quitGame")
 
 	
 func change_phase():
 	buttonAttack.disabled = true
-	
-	if playerAttacked:
-		phaseText.text = "Enemy Phase"
-	else:
+	if isPlayerPhase:
 		phaseText.text = "Player Phase"
+	else:
+		phaseText.text = "Enemy Phase"
 	
 	phaseText.show()
 	await wait(2.0)
 	phaseText.hide()
 	
-	if playerAttacked:
-		enemy_attack()
-	else:
+	if isPlayerPhase:
 		buttonAttack.disabled = false
+	else:
+		enemy_attack()
+		
+	isPlayerPhase = !isPlayerPhase
+
 
 func _on_button_attack_pressed():
 	buttonAttack.disabled = true
-	attack_calculation(Enemy, Player.get_attack(), hpLabelEnemy)
+	attack_calculation(Enemy, Player.get_attack(), hpLabelEnemy, true)
 	
 func enemy_attack():
-	attack_calculation(Player, Enemy.get_attack(), hpLabelPlayer)
+	attack_calculation(Player, Enemy.get_attack(), hpLabelPlayer, false)
 		
-func attack_calculation(defender, attack, label):
+func attack_calculation(defender, attack, label, isPlayer):
 	var finalHP = defender.get_health()
 	var damage = await damage_calculation(attack, defender.get_defense())
 	finalHP -= damage
@@ -80,32 +84,28 @@ func attack_calculation(defender, attack, label):
 	label.text = str(max(0,finalHP))
 	
 	if finalHP > 0:
-		if not playerAttacked:
+		if isPlayer:
 			GameStatus.set_active_enemy_hp(finalHP)
-			playerAttacked = !playerAttacked
-			change_phase()
-			
 		else:
 			GameStatus.set_active_player_hp(finalHP)
-			await wait(1.5)
-			get_tree().change_scene_to_file("res://Scenes/choose_combat.tscn")
+		change_phase()
 		
 	else:
 		buttonAttack.disabled = true
 		
-		if playerAttacked:
-			phaseText.text = "YOU LOST"
-			GameStatus.set_active_player_hp(0)
-		else:
+		if isPlayer:
 			phaseText.text = "ENEMY DEFEATED"
 			GameStatus.set_active_enemy_hp(0)
+		else:
+			phaseText.text = "YOU LOST"
+			GameStatus.set_active_player_hp(0)
 			
 		phaseText.show()
 		defender.death(damage)
 		save_result()
 		await wait(1.5)
 
-		if not playerAttacked:
+		if isPlayer:
 			var enemyHealthPool = 0
 			for enemy in GameStatus.enemies:
 				enemyHealthPool += GameStatus.enemies[enemy]["current_health"]
@@ -125,8 +125,7 @@ func attack_calculation(defender, attack, label):
 				get_tree().change_scene_to_file("res://Scenes/lose.tscn")
 			else:
 				get_tree().change_scene_to_file("res://Scenes/choose_combat.tscn")
-
-
+	
 func damage_calculation(att, def):
 	var damage = 0
 	damage = int(att * (1 - 0.1 * def) + randi_range(0, 3))
