6f8a3e4f8cbb7c274a25da087ecceef8d62d1f7e
Alejandro Gestoso <alegestor@alum.us.es>
2024-08-04 11:17:51 +0200

    testing

---
+++ tests/test_prueba.gd
@@ -0,0 +1,13 @@
+extends GutTest
+
+func test_positive_1():
+	var palabra = "palabra"
+	var tamanyo = palabra.length()
+	
+	assert_eq(tamanyo, 7, "El tamaño es igual")
+	
+func test_negative_1():
+	var palabra = "palabra"
+	var tamanyo = palabra.length()
+	
+	assert_eq(tamanyo, 8, "El tamaño no es igual")
