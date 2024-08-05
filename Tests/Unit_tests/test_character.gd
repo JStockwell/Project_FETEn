b92016695c4b468a582000225f550d8f1d7d463e
alegestor <alegestor@alum.us.es>
2024-08-05 21:10:37 +0200

    PFTN-1 feat: character tests

--- Tests/Unit_tests/test_character.gd
+++ Tests/Unit_tests/test_character.gd
@@ -1,11 +1,33 @@
-extends Node
+extends GutTest
 
+var Character = load("res://Scripts/character.gd")
+var char = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func before_each():
+	char = Character.new()
+	
+func after_each():
+	char.free()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func test_not_null():
+	assert_not_null(char)
+	
+func test_set_stats():
+	var stats = {
+		"name": "Player1",
+		"max_health": 24,
+		"attack": 16,
+		"dexterity": 16,
+		"defense": 6,
+		"movement": 5,
+		"ini_magic": 5,
+		"max_magic": 20,
+		"reg_magic": 5,
+		"range": 4
+	}
+	
+	char.set_stats(stats)
+	
+	var checker = char.get_stats()
+	
+	assert_eq(stats, checker, "Yo que se pibe v2")
