d101325d50805c01b71284c2884fb82525aab095
Alejandro Gestoso <alegestor@alum.us.es>
2024-08-09 11:42:36 +0200

    Update skill.gd

--- Scripts/Entities/skill.gd
+++ Scripts/Entities/skill.gd
@@ -10,31 +10,31 @@ var specialEffectFunc: String = null
 var canTargetAllies: bool = false
 
 func init_skill(args: Dictionary) -> void:
-   var validator = true
-   for variable in ["skill_name", "range", "cost"]:
-      if variable not in args.keys():
-         validator = false
+	var validator = true
+	for variable in ["skill_name", "range", "cost"]:
+		if variable not in args.keys():
+			validator = false
 
-   if validator:
-      skillName = args["skill_name"]
-      range = args["range"]
-      cost = args["cost"]
-      
-      if "spa" in args.keys():
-         spa = args["spa"]
-      
-      if "sef" in args.keys():
-         specialEffectFunc = args["sef"]
-         
-      if "cta" in args.keys():
-         canTargetAllies = args["cta"] 
-         
+	if validator:
+		skillName = args["skill_name"]
+		range = args["range"]
+		cost = args["cost"]
+	  
+		if "spa" in args.keys():
+			spa = args["spa"]
+	  
+		if "sef" in args.keys():
+			specialEffectFunc = args["sef"]
+		 
+		if "cta" in args.keys():
+			canTargetAllies = args["cta"] 
+		 
 func get_skill() -> Dictionary:
-    return {
-       "skill_name": skillName,
-       "range": range,
-       "cost": cost,
-       "spa": spa,
-       "sef": specialEffectFunc,
-       "cta": canTargetAllies
-    }        
+	return {
+		"skill_name": skillName,
+		"range": range,
+		"cost": cost,
+		"spa": spa,
+		"sef": specialEffectFunc,
+		"cta": canTargetAllies
+	}        
