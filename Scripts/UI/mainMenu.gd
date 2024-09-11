91c6b701b575ca1ba181653a85f270ddfefd0399
JStockwell <jstockwellmendoza@gmail.com>
2024-09-11 15:02:51 +0200

    TODO: unlock update

--- Scripts/UI/mainMenu.gd
+++ Scripts/UI/mainMenu.gd
@@ -5,10 +5,16 @@ var enemySet = Utils.read_json("res://Assets/json/enemies.json")
 
 func _ready():
 	GameStatus.load_save()
+	update_unlocks()
 	GameStatus.set_playable_characters(playableCharacters)
 	GameStatus.set_enemy_set(enemySet)
 	GameStatus.party = {}
 	CombatMapStatus.isStartCombat = true
+
+func update_unlocks() -> void:
+	var updateFlag = false
+	var tempSave = GameStatus.save.duplicate()
+	pass
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/UI/characterSelect.tscn")
